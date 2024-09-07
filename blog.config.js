2891ad9994ec77bb85e4047ef3a5c8040f67ed96
McFawwaz <mailmcfawwaz@gmail.com>
2024-09-07 21:35:44 +0700

    erase description

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Mochammad Fawwaz Islami',
   email: 'mailmcfawwaz@gmail.com',
   link: 'https://mcfawwaz.com',
-  description: 'a personal space where I share my thoughts, experiences, and passions.',
+  description: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Jakarta', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
